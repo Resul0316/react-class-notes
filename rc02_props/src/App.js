cbd0547803d583dcc28c1e69c0b3f8be8bb0d31c
Resul0316 <93797905+Resul0316@users.noreply.github.com>
2022-06-15 09:56:51 +0200

    Update App.js

--- rc02_props/src/App.js
+++ rc02_props/src/App.js
@@ -14,6 +14,7 @@ function App() {
         img="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
         tel="55555555"
       />
+    
       <Person
         name="Canan Can"
         img="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
